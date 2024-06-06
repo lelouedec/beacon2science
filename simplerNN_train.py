01b7b3a49a18f6393d79d78a05a43f0f0c17038e
lelouedec <justin.lelouedec@gmail.com>
2024-06-06 18:45:55 +0200

    Update simplerNN_train.py

--- simplerNN_train.py
+++ simplerNN_train.py
@@ -25,24 +25,6 @@ import sys
 
 from torch.utils.tensorboard import SummaryWriter
 
-class TVLoss(nn.Module):
-    def __init__(self, tv_loss_weight=1):
-        super(TVLoss, self).__init__()
-        self.tv_loss_weight = tv_loss_weight
-
-    def forward(self, x):
-        batch_size = x.size()[0]
-        h_x = x.size()[2]
-        w_x = x.size()[3]
-        count_h = self.tensor_size(x[:, :, 1:, :])
-        count_w = self.tensor_size(x[:, :, :, 1:])
-        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
-        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
-        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
-
-    @staticmethod
-    def tensor_size(t):
-        return t.size()[1] * t.size()[2] * t.size()[3]
 
 
 def train_discriminator(discriminator,data,sr,adversarial_looser,device,target):
@@ -77,7 +59,7 @@ def train(config):
     print("THIS WILL RUN ON DEVICE:", device)
 
     model = unet2.ResUnet(2,full_size=config["full_size"])
-    discriminator = ESRGAN.Discriminator(2,2,64)
+    discriminator = ESRGAN.Discriminator(2,2,64,full_size=config["full_size"])
 
     if(config["load_models"]==True):
         model.load_state_dict(torch.load(config["generator"],map_location=torch.device('cpu')))
