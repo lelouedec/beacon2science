14dd96d1d67e7288a7663b0d246980f4ad78e2b7
lelouedec <justin.lelouedec@gmail.com>
2024-06-30 16:31:41 +0200

    asdasd

--- TestNN2.py
+++ TestNN2.py
@@ -15,6 +15,7 @@ import cv2
 from datetime import datetime,timedelta
 import matplotlib.pyplot as plt 
 import os 
+from astropy.io import fits
 
 
 def test():
@@ -41,7 +42,7 @@ def test():
     dt1 = torch.ones((1)).to(device) * 0.33
     dt2 = torch.ones((1)).to(device) * 0.66
 
-    imgs_paths = natsorted(glob.glob("/Volumes/Data_drive/res_model_final3/*"))
+    imgs_paths = natsorted(glob.glob("../res_model_final3/*"))
     with torch.no_grad():
         for p in range(0,len(imgs_paths)-1):
 
@@ -62,17 +63,54 @@ def test():
 
                 timeout1 = time1+timedelta(seconds=diff_time*0.33)
                 timeout2 = time1+timedelta(seconds=diff_time*0.66)
-                name1    = str(timeout1.year) +"-"+ '%02d' %timeout1.month +"-"+'%02d' % timeout1.day+"T"+'%02d' %timeout1.hour+"-"+'%02d' % timeout1.minute+"-"+'%02d' %timeout1.second
-                name2    = str(timeout2.year) +"-"+ '%02d' %timeout2.month +"-"+'%02d' % timeout2.day+"T"+'%02d' %timeout2.hour+"-"+'%02d' % timeout2.minute+"-"+'%02d' %timeout2.second
-                os.system("mv "+imgs_paths[p]+" /Volumes/Data_drive/res_NN2_1/"+imgs_paths[p].split("/")[-1])
+                name1    = str(timeout1.year) +"-"+ '%02d' %timeout1.month +"-"+'%02d' % timeout1.day+"T"+'%02d' %timeout1.hour+"-"+'%02d' % timeout1.minute+"-"+'%02d' %timeout1.second+".000"
+                name2    = str(timeout2.year) +"-"+ '%02d' %timeout2.month +"-"+'%02d' % timeout2.day+"T"+'%02d' %timeout2.hour+"-"+'%02d' % timeout2.minute+"-"+'%02d' %timeout2.second+".000"
+                
 
-                img = Image.fromarray((output1*255.0).astype(np.uint8))
-                img.save("/Volumes/Data_drive/res_NN2_1/"+name1+".png")
+                ## move img input to fits file with correct header
+                name = '../test_fits/'+imgs_paths[p].split("/")[-1][:-3]+"fts"
+                filea = fits.open(name)
+                hdr = filea[0].header
+                fits.writeto("../enhanced_fits/"+imgs_paths[p].split("/")[-1][:-3]+"fts", S1[0,0,:,:].cpu().numpy().astype(np.float32), hdr, output_verify='silentfix', overwrite=True)
+                filea.close()
 
-                img = Image.fromarray((output2*255.0).astype(np.uint8))
-                img.save("/Volumes/Data_drive/res_NN2_1/"+name2+".png")
+                filea = fits.open('../test_fits/'+imgs_paths[p+1].split("/")[-1][:-3]+"fts")
+                hdr2 = filea[0].header
+                filea.close()
+
+                hdr1_2 = hdr.copy()
+                hdr2_2 = hdr.copy()
+
+
+                crval1 = hdr["crval1a"] + (hdr2["crval1a"] - hdr["crval1a"])*0.33
+                crval2 = hdr["crval2a"] + (hdr2["crval2a"] - hdr["crval2a"])*0.33
+                hdr1_2["crval1a"] = crval1
+                hdr1_2["crval2a"] = crval2
+                hdr1_2["DATE-END"] = name1
+
+
+                crval1 = hdr["crval1a"] + (hdr2["crval1a"] - hdr["crval1a"])*0.66
+                crval2 = hdr["crval2a"] + (hdr2["crval2a"] - hdr["crval2a"])*0.66
+                hdr2_2["crval1a"] = crval1
+                hdr2_2["crval2a"] = crval2
+                hdr2_2["DATE-END"] = name2
+                
+                fits.writeto("../enhanced_fits/"+name1+".fts", output1.astype(np.float32), hdr, output_verify='silentfix', overwrite=True)
+                fits.writeto("../enhanced_fits/"+name2+".fts", output2.astype(np.float32), hdr, output_verify='silentfix', overwrite=True)
+
             else:
-                os.system("mv "+imgs_paths[p]+" /Volumes/Data_drive/res_NN2_1/"+imgs_paths[p].split("/")[-1])
+                name = '../test_fits/'+imgs_paths[p].split("/")[-1][:-3]+"fts"
+                filea = fits.open(name)
+                hdr = filea[0].header
+                fits.writeto("../enhanced_fits/"+imgs_paths[p].split("/")[-1][:-3]+"fts", S1[0,0,:,:].cpu().numpy().astype(np.float32), hdr, output_verify='silentfix', overwrite=True)
+                filea.close()
+
+
+        name = '../test_fits/'+imgs_paths[p+1].split("/")[-1][:-3]+"fts"
+        filea = fits.open(name)
+        hdr = filea[0].header
+        fits.writeto("../enhanced_fits/"+imgs_paths[p+1].split("/")[-1][:-3]+"fts", S2[0,0,:,:].cpu().numpy().astype(np.float32), hdr, output_verify='silentfix', overwrite=True)
+        filea.close()
 
            
 
