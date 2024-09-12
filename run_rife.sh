c3f70b7b35977deb8ccf71c13cdf52d853e1864c
lelouedec <justin.lelouedec@gmail.com>
2024-09-12 17:08:53 +0200

    Create run_rife.sh

---
+++ run_rife.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J Train_RIFE                           #use -N only if you use both GPUs on the nodes, otherwise leave this line out
+#SBATCH --partition zen2_0256_a40x2
+#SBATCH --qos zen2_0256_a40x2
+#SBATCH --gres=gpu:1                   #or --gres=gpu:1 if you only want to use half a node
+#SBATCH --output=output.txt
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-type=fail         # send email if job fails
+#SBATCH --mail-user=justin.lelouedec@gmail.com
+
+
+module purge
+
+nvidia-smi
+module load miniconda3
+
+eval "$(conda shell.bash hook)"
+
+conda activate gan
+conda list 
+
+
+python RIFE.py configs/config_rife.json
