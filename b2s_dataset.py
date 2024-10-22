b30701547e848c35a9a4c38f1e48d19ada1c5332
lelouedec <justin.lelouedec@gmail.com>
2024-10-22 08:42:27 +0200

    Update b2s_dataset.py

--- b2s_dataset.py
+++ b2s_dataset.py
@@ -13,59 +13,50 @@ import pickle
 from scipy.ndimage import shift
 import json
 from datetime import datetime
+from astropy.io import fits
+import random
 
 class FinalDataset(Dataset):
-    def __init__(self, l_resolution=128, r_resolution=256,path="../finals_test",training=True,validation=False):
+    def __init__(self, l_resolution=128, r_resolution=256,path="../Dataset/training",training=True,validation=False,small=False):
         self.l_res = l_resolution
         self.r_res = r_resolution
         self.path = path
        
        
         if(training or validation):
-            with open("dataset_bis.json", "r") as final:
+            with open("dataset_train_final.json", "r") as final:
                 self.data_json =  json.load(final)
         else:
             with open("dataset_test.json", "r") as final:
                 self.data_json =  json.load(final)
         
+        
+        np.random.seed(seed=42)
+        all_indices = np.arange(0,len(self.data_json))
+        indices_train = np.array(random.sample(range(0,all_indices.shape[0]), int(all_indices.shape[0]*0.90)))
+        validation_indices = np.setdiff1d(all_indices, indices_train,assume_unique=True)
+
+
         if(validation):
-            self.data_json = self.data_json[int(len(self.data_json)*0.9): ]
+            self.data_json = [self.data_json[i] for i in validation_indices]
         elif(training and not validation):
-            self.data_json = self.data_json[:int(len(self.data_json)*0.9)]
-
-
+            self.data_json = [self.data_json[i] for i in indices_train]
+        
+        print(len(self.data_json))
 
     def __len__(self):
         return len(self.data_json)
+    
+   
+
 
     def __getitem__(self, index):
 
-        distances  = torch.ones((self.r_res,self.r_res))
-
-        grid_x, grid_y = torch.meshgrid(torch.arange(0,self.r_res), torch.arange(0,self.r_res), indexing='ij')
-
-        idxs = torch.dstack([grid_x,grid_y]).reshape((self.r_res*self.r_res,2))
-
-
-        distances  = torch.sqrt(( (idxs[:,0]-(self.r_res/2))**2   + idxs[:,1]**2)).reshape((self.r_res,self.r_res))
-
-        distances = (distances-distances.min())/(distances.max()-distances.min())
-
-        distances = distances
-
-        ### pre conjecture 
-        if(int(self.data_json[index]["s1"].split("-")[0])<=2015 and int(self.data_json[index]["s1"].split("-")[1])<7):
-            distances = torch.flip(distances, [1])
-
-    
-        self.clahe_b = cv2.createCLAHE(clipLimit=10,tileGridSize=(10,10))
-        self.clahe_s = cv2.createCLAHE(clipLimit=10,tileGridSize=(10,10))
        
-
-        s1 = self.clahe_s.apply(np.asarray(Image.open(self.path+"images_science/"+self.data_json[index]["s1"]).convert("L")))/255.0
-        s2 = self.clahe_s.apply(np.asarray(Image.open(self.path+"images_science/"+self.data_json[index]["s2"]).convert("L")))/255.0
-        b1 = self.clahe_b.apply(np.asarray(Image.open(self.path+"images_beacon/"+self.data_json[index]["b1"]).convert("L")))/255.0
-        b2 = self.clahe_b.apply(np.asarray(Image.open(self.path+"images_beacon/"+self.data_json[index]["b2"]).convert("L")))/255.0
+        s1 = np.asarray(Image.open(self.path+"science/"+self.data_json[index]["s1"].replace("fts",".png")).convert("L"))/255.0
+        s2 = np.asarray(Image.open(self.path+"science/"+self.data_json[index]["s2"].replace("fts",".png")).convert("L"))/255.0
+        b1 = np.asarray(Image.open(self.path+"beacon/"+self.data_json[index]["b1"].replace("fts",".png")).convert("L"))/255.0
+        b2 = np.asarray(Image.open(self.path+"beacon/"+self.data_json[index]["b2"].replace("fts",".png")).convert("L"))/255.0
         
 
         shift_arr = np.array(self.data_json[index]["shift"])
@@ -75,25 +66,6 @@ class FinalDataset(Dataset):
         diff1   = np.float32(s2-shift(s1,shift_arr, order=2,mode='nearest',prefilter=False))
         diff2   = np.float32(s1-shift(s2,-1.0*shift_arr, order=2,mode='nearest',prefilter=False))
         tr1 = np.array([shift_arr[1],shift_arr[0]])
-
-        # time1 = datetime.strptime(self.data_json[index]["s1"][:-8], '%Y-%m-%dT%H-%M-%S')
-        # time2 = datetime.strptime(self.data_json[index]["s2"][:-8], '%Y-%m-%dT%H-%M-%S')
-        # diff_time = (time2-time1).total_seconds()
-        # ratios = []
-        # for v in self.data_json[index]["mid"]:
-        #     timev = datetime.strptime(v[:-8], '%Y-%m-%dT%H-%M-%S')
-        #     print(time1,timev)
-        #     diffv = (timev-time1).total_seconds()
-        #     ratios.append(diffv/diff_time)
-        # print(ratios)
-        # exit()
-
-      
-
-
-        # diff1_b,tr1_b = self.difference(b2,b1 ,np.array(shifts))
-        # diff2_b,tr2_b = self.difference(b1,b2,(-1*np.array(shifts))/4)
-
 
 
         b1  =  cv2.resize(b1, (self.l_res , self.l_res),interpolation = cv2.INTER_CUBIC)
@@ -116,7 +88,6 @@ class FinalDataset(Dataset):
                 "diff1":torch.tensor(diff1).unsqueeze(0).float(),
                 "diff2":torch.tensor(diff2).unsqueeze(0).float(),
                 "tr1":tr1,           
-                "distances":distances
                 }
     
 
